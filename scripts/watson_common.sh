17ffa3499997653bdac03d5d6c0bbdd20b1ff4b4
totemo <TotemoResearch@gmail.com>
2013-05-03 15:03:06 +0930

    Recompiled for 1.5.2.

--- scripts/watson_common.sh
+++ scripts/watson_common.sh
@@ -1,8 +1,8 @@
 #------------------------------------------------------------------------------
 # These lines need to change with the Minecraft and MCP versions.
 
-MCP_VER=744
-MC_VER=1.5.1
+MCP_VER=751
+MC_VER=1.5.2
 
 #------------------------------------------------------------------------------
 # These lines only change if your directory setup is different to mine.
